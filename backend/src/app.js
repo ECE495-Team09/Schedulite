2ef0f4d9af605254072c650c3320b3d692821007
riccil <riccil@purdue.edu>
2026-02-05 15:26:43 -0500

    new branch

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,24 @@
 // backend/src/app.js
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 //Routes
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import groupRoutes from "./routes/users.js";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(helmet());
 app.use(cors({origin: "http://localhost:5173" }));
 
 // Routes
 app.get("/health", (_req, res) => {res.json({ ok: true });});
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/groups", groupRoutes);
 
 export default app;
